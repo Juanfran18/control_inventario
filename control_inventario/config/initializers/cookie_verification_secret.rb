0c53990921482d66b64865ec9f551ef911e0c7c9
Juanfran18 <guatecremas_gp@yahoo.es>
2012-09-13 20:36:14 -0600

    este es la inicializacion del repositori y proyecto

---
+++ control_inventario/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'fca37a182d68bda6775f54eb4f878588fa4e7c63a8d762e6800b7f7dad69c974829521cb4f9b471eb2e07cee7cfb1ac26fca9345d72910cb8c7db000e16bc364';
